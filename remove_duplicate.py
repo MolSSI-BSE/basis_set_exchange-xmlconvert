339bbbc37c64f31823859ef0a42c12dc6b5537bf
Benjamin Pritchard <ben@bennyp.org>
2017-12-05 19:16:30 -0500

    Work on duplicate script

--- remove_duplicate.py
+++ remove_duplicate.py
@@ -8,6 +8,17 @@ import sys
 import bse
 
 
+# Is a dictionary a subset of another?
+# https://stackoverflow.com/questions/9323749/python-check-if-one-dictionary-is-a-subset-of-another-larger-dictionary
+def is_subset(subset, superset):
+    # For dicts
+    #return all(item in superset.items() for item in subset.items())
+
+    # For lists
+    return all(item in superset for item in subset)
+    
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('source_file', help='File to use as the master', type=str)
 args = parser.parse_args()
@@ -33,8 +44,9 @@ sdata = bse.read_json_by_path(args.source_file)
 sname = os.path.splitext(args.source_file)[0]
 selements = sdata['basisSetElements']
 
-# Map of what we replaced
+# Map of what we replaced or added
 replaced_map = {}
+added_map = {}
 
 for cfile in all_component_files:
     print("Comparing with {}".format(cfile))
@@ -64,6 +76,17 @@ for cfile in all_component_files:
             # Remove from the candidate
             celements.pop(k)
 
+        elif is_subset(sel['elementElectronShells'], cel['elementElectronShells']):
+            changed = True
+
+            if not cname in added_map:
+                added_map[cname] = []
+
+            added_map[cname].append(k)
+
+            # remove only the equivalent shells
+            celements[k]['elementElectronShells'] = [ x for x in cel['elementElectronShells'] if not x in sel['elementElectronShells'] ]
+
 
     # rewrite the candidate file        
     if changed:
@@ -73,6 +96,9 @@ for cfile in all_component_files:
 # print out the replacement map
 for k,v in replaced_map.items():
     print("Replaced in {}: {}".format(k, v))
+for k,v in added_map.items():
+    print("Partially replaced in {}: {}".format(k, v))
+
 
 # Now go through all the atom basis files, doing replacements
 for afile in all_atom_files:
@@ -84,6 +110,10 @@ for afile in all_atom_files:
         for i,c in enumerate(v['elementComponents']):
             if c in replaced_map and k in replaced_map[c]:
                 v['elementComponents'][i] = sname
+                changed = True
+
+            if c in added_map and k in added_map[c]:
+                v['elementComponents'].append(sname)
                 changed = True
 
     if changed:
